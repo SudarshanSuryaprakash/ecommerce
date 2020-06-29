19d16353917529f210d7bb3460d991bcc3aded34
Sudarshan Suryaprakash <sudarshan@Sudarshans-MacBook-Air.local>
2020-06-29 13:40:22 +0100

    final

---
+++ ecommerce-front/src/core/fixedPrices.js
@@ -0,0 +1,32 @@
+export const prices = [
+  {
+    _id: 0,
+    name: 'Any',
+    array: [],
+  },
+  {
+    _id: 1,
+    name: '₹100 to ₹199',
+    array: [100, 199],
+  },
+  {
+    _id: 2,
+    name: '₹200 to ₹299',
+    array: [200, 299],
+  },
+  {
+    _id: 3,
+    name: '₹300 to ₹399',
+    array: [300, 399],
+  },
+  {
+    _id: 4,
+    name: '₹400 to ₹499',
+    array: [400, 499],
+  },
+  {
+    _id: 5,
+    name: 'More than ₹500',
+    array: [500, 9999],
+  },
+];
